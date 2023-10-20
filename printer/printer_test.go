c1b804082f4c8c61610a7f0ab8f5d46da987143e
erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>
2023-10-20 14:07:21 -0400

    require that block label strings use a double quoted string
    add testing pattern for parsing river with errors
    
    Signed-off-by: erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>

--- printer/printer_test.go
+++ printer/printer_test.go
@@ -23,17 +23,13 @@ func TestPrinter(t *testing.T) {
 		if strings.HasSuffix(path, ".in") {
 			inputFile := path
 			expectFile := strings.TrimSuffix(path, ".in") + ".expect"
-
-			inputBB, err := os.ReadFile(inputFile)
-			require.NoError(t, err)
-			expectBB, err := os.ReadFile(expectFile)
-			require.NoError(t, err)
+			expectErrorFile := strings.TrimSuffix(path, ".in") + ".error"
 
 			caseName := filepath.Base(path)
 			caseName = strings.TrimSuffix(caseName, ".in")
 
 			t.Run(caseName, func(t *testing.T) {
-				testPrinter(t, inputBB, expectBB)
+				testPrinter(t, inputFile, expectFile, expectErrorFile)
 			})
 		}
 
@@ -41,13 +37,41 @@ func TestPrinter(t *testing.T) {
 	})
 }
 
-func testPrinter(t *testing.T, input, expect []byte) {
-	f, err := parser.ParseFile(t.Name()+".rvr", input)
+func testPrinter(t *testing.T, inputFile string, expectFile string, expectErrorFile string) {
+	inputBB, err := os.ReadFile(inputFile)
+	require.NoError(t, err)
+
+	f, err := parser.ParseFile(t.Name()+".rvr", inputBB)
+	if expectedError := getExpectedErrorMessage(t, expectErrorFile); expectedError != "" {
+		require.Error(t, err)
+		require.Contains(t, err.Error(), expectedError)
+		return
+	}
+
+	expectBB, err := os.ReadFile(expectFile)
 	require.NoError(t, err)
 
 	var buf bytes.Buffer
 	require.NoError(t, printer.Fprint(&buf, f))
 
-	trimmed := strings.TrimRightFunc(string(expect), unicode.IsSpace)
+	trimmed := strings.TrimRightFunc(string(expectBB), unicode.IsSpace)
 	require.Equal(t, trimmed, buf.String(), "%s", buf.String())
+}
+
+// getExpectedErrorMessage will retrieve an optional expected error message for the test.
+func getExpectedErrorMessage(t *testing.T, errorFile string) string {
+	if _, err := os.Stat(errorFile); err == nil {
+		errorBytes, err := os.ReadFile(errorFile)
+		require.NoError(t, err)
+		errorsString := string(normalizeLineEndings(errorBytes))
+		return errorsString
+	}
+
+	return ""
+}
+
+// normalizeLineEndings will replace '\r\n' with '\n'.
+func normalizeLineEndings(data []byte) []byte {
+	normalized := bytes.ReplaceAll(data, []byte{'\r', '\n'}, []byte{'\n'})
+	return normalized
 }
